63d992e9b0f8efe9b8f9b32382fc5298cc56e9a8
andres <78615899+andreshurtadoo@users.noreply.github.com>
2022-12-12 10:15:34 -0400

    aqui se crea la matriz

---
+++ goles.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pMundial;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class goles {
+    
+    public static String[][] data(){
+        String vec[][] = {
+            {"1","2","3","4","5"},
+            {"Kylian Mbappé","Olivier Giroud","Lionel Messi","Julián Álvarez","Youssef En-Nesyri"},
+            {"Francia", "Francia","Argentina","Argentina","Marruecos"},
+            {"5","4","4","2","2"},
+            {"0","0","2","0","0"}
+        };
+        
+        return vec;
+    }
+}
